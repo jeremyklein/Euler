fd45dc79544b7f3a47ba19e358d1a3ce66710286
jeremyklein <kleinjj@lafayette.edu>
2014-12-02 07:32:54 -0500

    problem2 solution --needs optimization

--- problem2.py
+++ problem2.py
@@ -1,13 +1,18 @@
+def fib(n):
+	if n<2:
+		return n
+	return fib(n-2)+fib(n-1)
+
+
 def main():
+	number=0
+	i=0
 	sum=0
-	fib=1
-	while fib<4000000:
-		fib+=fib
-		print fib
-		if fib%2==0:
-			sum=sum+fib
-			print fib
-			print sum
+	while number<4000000:
+		number=fib(i)
+		if number%2==0:
+			sum+=number
+		i+=1
 	print sum
 
 main()
